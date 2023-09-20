177f17092bc6f756b3b9dad9dc4a2a1a1111f376
Dimitar <47664474+eggressive@users.noreply.github.com>
2023-09-20 15:04:50 +0200

    #14 Create S3 bucket

--- main.tf
+++ main.tf
@@ -4,17 +4,29 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 10
   upper            = false
-  special          = false
+  special          = true
+  override_special = "-."
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
