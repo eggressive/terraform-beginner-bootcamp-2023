0990887e97797d675707ecb265864b82eac9fa37
Dimitar Dimitrov <mitko.69@gmail.com>
2023-09-21 18:13:18 +0000

    Set TF alias #26

---
+++ bin/tfalias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "alias tf='terraform'" >> ~/.bash_aliases
+echo "alias tfapply='terraform apply --auto-approve'" >> ~/.bash_aliases
+echo "alias tfdestroy='terraform destroy --auto-approve'" >> ~/.bash_aliases
