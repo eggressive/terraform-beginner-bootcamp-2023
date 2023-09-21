20a8dc7231796095d917a6f9228c54e6697abedf
Dimitar Dimitrov <mitko.69@gmail.com>
2023-09-21 11:16:42 +0000

    Improve tflogin.sh script #22

--- bin/tflogin.sh
+++ bin/tflogin.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-#if [ -z "$TF_CLI_CONFIG_FILE" ]; then
-#  echo "Error: TF_CLI_CONFIG_FILE environment variable is not set"
-#  exit 1
-#fi
+# Define target directory and file
+TARGET_DIR="$HOME/.terraform.d"
+TF_CLI_CONFIG_FILE="$TARGET_DIR/credentials.tfrc.json"
+
+if [ -z "$TF_TOKEN" ]; then
+  echo "Error: $TF_TOKEN environment variable is not set"
+  exit 1
+fi
+
+# Check if directory exists, if not, create it
+if [ ! -d "$TARGET_DIR" ]; then
+    mkdir -p "$TARGET_DIR"
+    echo "${TARGET_DIR} has been created."
+fi
 
 cat <<EOF > "$TF_CLI_CONFIG_FILE"
 {
@@ -14,3 +24,5 @@ cat <<EOF > "$TF_CLI_CONFIG_FILE"
   }
 }
 EOF
+
+echo "${TF_CLI_CONFIG_FILE} has been created."
