7b00b78a9bd63ba853e10e63533c582eaaa7da4f
Dimitar Dimitrov <mitko.69@gmail.com>
2023-09-21 11:00:14 +0000

    Fix typo in tflogin.sh #22

--- bin/tflogin.sh
+++ bin/tflogin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$TF_CLI_CONFIG_FILE" ]; then
-  echo "Error: TF_CLI_CONFIG_FILE environment variable is not set"
-  exit 1
-fi
+#if [ -z "$TF_CLI_CONFIG_FILE" ]; then
+#  echo "Error: TF_CLI_CONFIG_FILE environment variable is not set"
+#  exit 1
+#fi
 
 cat <<EOF > "$TF_CLI_CONFIG_FILE"
 {
