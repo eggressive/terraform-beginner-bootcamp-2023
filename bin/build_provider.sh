a0cc7ff5eba320d9d24a24a39f17d054be51d846
Dimitar Dimitrov <mitko.69@gmail.com>
2023-10-10 12:12:06 +0000

    build_provider improved

--- bin/build_provider.sh
+++ bin/build_provider.sh
@@ -3,34 +3,19 @@
 ## https://servian.dev/terraform-local-providers-and-registry-mirror-configuration-b963117dfffa
 ## https://www.terraform.io/docs/cli/config/config-file.html#provider-installation
 
-PLUGIN_DIR=$HOME/.terraform.d/plugins/local.providers/local/terratowns/1.0.0
-PLUGIN_NAME=terraform-provider-terratowns_v1.0.0
+PLUGIN_DIR="/home/gitpod/.terraform.d/plugins/local.providers/local/terratowns/1.0.0/"
+PLUGIN_NAME="terraform-provider-terratowns_v1.0.0"
 
 cd $PROJECT_ROOT/terraform-provider-terratowns
 cp $PROJECT_ROOT/terraformrc $HOME/.terraformrc
 
-if [ -d "$HOME/.terraform.d/plugins/" ]; then
-    rm -rf $HOME/.terraform.d/plugins/
-fi
-
-if [ -d "$PROJECT_ROOT/.terraform" ]; then
-    rm -rf $PROJECT_ROOT/.terraform
-fi
-
-if [ -f "$PROJECT_ROOT/.terraform.lock.hcl" ]; then
-    rm -rf $PROJECT_ROOT/.terraform.lock.hcl
-fi
-
-echo "Compiling..."
+cd $PROJECT_ROOT/terraform-provider-terratowns
+cp $PROJECT_ROOT/terraformrc /home/gitpod/.terraformrc
+rm -rf /home/gitpod/.terraform.d/plugins
+rm -rf $PROJECT_ROOT/.terraform
+rm -rf $PROJECT_ROOT/.terraform.lock.hcl
 go build -o $PLUGIN_NAME
-echo "Compiling done."
-
-# Create directories if they don't exist
-mkdir -p $PLUGIN_DIR/linux_amd64
-mkdir -p $PLUGIN_DIR/x86_64-apple-darwin
-mkdir -p $PLUGIN_DIR/x86_64
-
-# Copy binary to all directories
-for dir in linux_amd64 x86_64-apple-darwin x86_64; do
-    cp $PLUGIN_NAME $PLUGIN_DIR/$dir
-done
+mkdir -p $PLUGIN_DIR/x86_64/
+mkdir -p $PLUGIN_DIR/linux_amd64/
+cp $PLUGIN_NAME $PLUGIN_DIR/x86_64
+cp $PLUGIN_NAME $PLUGIN_DIR/linux_amd64
