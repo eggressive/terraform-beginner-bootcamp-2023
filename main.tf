339631f999496d17201fa9e4547bee47632092e9
Dimitar Dimitrov <mitko.69@gmail.com>
2023-10-10 18:21:01 +0000

    Terratown testing against real backend #57

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ terraform {
 }
 
 provider "terratowns" {
-  #endpoint  = "https://terratowns.cloud/api"
-  endpoint = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.user_uuid
+  token     = var.terratowns_access_token
 }
 
 resource "terratowns_home" "home" {
@@ -21,18 +20,17 @@ resource "terratowns_home" "home" {
   Here, you can learn about the movie's groundbreaking special effects,
   its thought-provoking philosophical themes, and its enduring cultural impact.
 EOF
-  town = "video-valley"
-  #domain_name = module.terrahouse_aws.cdn_website_url
-  domain_name = "something1.cloudfront.net"
+  town = "missingo"
+  domain_name = module.terrahouse_aws.cdn_website_url
+  #domain_name = "test212.cloudfront.net"
   content_version = 1
 }
 
-
-/* module "terrahouse_aws" {
+module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version = var.content_version
-} */
+  assets_path = var.assets_path
+}
--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,7 +1,6 @@
-# Output definition for the bucket name
 output "bucket_name" {
-  description = "The name of the S3 bucket for the static website."
-  value       = aws_s3_bucket.s3bucket.bucket
+  description = "The name of the S3 bucket."
+  value       = aws_s3_bucket.s3bucket.id
 }
 
 output "s3_website_url" {
